a1f6c5be49ee281ed5d8ccb8f1ee1183eb796e99
yevhen-danchenko <yevhen.danchenko@ringteam.com>
2022-06-05 19:38:08 +0300

    status codes

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,20 @@ from app.controller import get_cars, get_car_by_id, store_car, delete_car, get_d
 from app.data.models import Car, Dealer, is_valid
 from app.utils import json_response
 
+SC_OK = 200
+SC_CREATED = 201
+SC_BAD_REQUEST = 400
+SC_NOT_FOUND = 404
 
-@app.errorhandler(404)
+
+@app.errorhandler(SC_NOT_FOUND)
 def not_found(error):
-    return json_response({'error': 'Not found'}, 404)
+    return json_response({'error': 'Not found'}, SC_NOT_FOUND)
 
 
-@app.errorhandler(400)
+@app.errorhandler(SC_BAD_REQUEST)
 def bad_request(error):
-    return json_response({'error': 'Bad request'}, 400)
+    return json_response({'error': 'Bad request'}, SC_BAD_REQUEST)
 
 
 @app.route("/dealers", methods=["GET"])
@@ -26,15 +31,15 @@ def add_dealer():
     json = request.get_json()
     if is_valid(Dealer(), json):
         store_dealer(Dealer(**json))
-        return json_response({'result': 'Success'}, 201)
+        return json_response({'result': 'Success'}, SC_CREATED)
     else:
-        abort(400)
+        abort(SC_BAD_REQUEST)
 
 
 @app.route("/dealers/<int:dealer_id>", methods=["DELETE"])
 def remove_dealer(dealer_id: int):
     delete_dealer(dealer_id)
-    return json_response({'result': 'Success'}, 200)
+    return json_response({'result': 'Success'}, SC_OK)
 
 
 @app.route("/cars", methods=["GET"])
@@ -47,7 +52,7 @@ def list_cars():
 @app.route("/cars/<int:car_id>", methods=["GET"])
 def get_car(car_id: int):
     car = get_car_by_id(car_id)
-    return json_response(car[0]) if len(car) > 0 else abort(404)
+    return json_response(car[0]) if len(car) > 0 else abort(SC_NOT_FOUND)
 
 
 @app.route("/cars", methods=["POST"])
@@ -55,12 +60,12 @@ def add_car():
     json = request.get_json()
     if is_valid(Car(), json):
         store_car(Car(**json))
-        return json_response({'result': 'Success'}, 201)
+        return json_response({'result': 'Success'}, SC_CREATED)
     else:
-        abort(400)
+        abort(SC_BAD_REQUEST)
 
 
 @app.route("/cars/<int:car_id>", methods=["DELETE"])
 def remove_car(car_id: int):
     delete_car(car_id)
-    return json_response({'result': 'Success'}, 200)
+    return json_response({'result': 'Success'}, SC_OK)
